b860669f30321cd41548f9ab9e1963288c1c5b5c
Pierpaolo <plucarelli.94@gmail.com>
2018-02-27 09:32:08 +0000

    dgx

--- main.py
+++ main.py
@@ -40,38 +40,6 @@ def test():
 	print('game over')
 	playerA.save_Qtable()
 
-def test_vs_old():
-	numActions = env.n_actions
-	playerA = QLearn(actions=list(range(numActions)))
-	playerB = RandomPlayer(numActions-1)
-	start_time = time.time()
-	for episode in range(N_EPISODES):
-		# initial observation
-		observation = env.reset()
-		# print(str(episode))
-		if(episode % 500 == 0):
-			print(str(float(episode) / N_EPISODES * 100) + "%")
-		while True:
-			# RL choose action based on observation
-			actionA = playerA.choose_action(str(observation))
-			actionB = playerB.choose_action(str(observation))
-
-			# RL take action and get next observation and reward
-			observation_, reward, done = env.step(actionA, actionB)
-			#print(observation_)
-
-			# RL learn from this transition
-			playerA.learn(str(observation), actionA, reward, str(observation_), done)
-			# swap observation
-			observation = observation_
-
-			# break while loop when end of this episode
-			if done:
-				break
-	# end of game
-	print "My program took", time.time() - start_time, "to run"
-	print('game over')
-	playerA.save_Qtable()
 
 def run_optimal():
 	vis = Visualiser(env, 80)
